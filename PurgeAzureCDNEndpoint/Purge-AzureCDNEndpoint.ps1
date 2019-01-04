31d78067538767317daf11a1ff3e708e6c45dfe5
Fabien Lavocat <fabien.lavocat@live.com>
2019-01-04 08:59:24 -0800

    Add the possibility to provide more than one purge content #4

--- PurgeAzureCDNEndpoint/Purge-AzureCDNEndpoint.ps1
+++ PurgeAzureCDNEndpoint/Purge-AzureCDNEndpoint.ps1
@@ -15,9 +15,12 @@ try {
     # preference to continue. An implication of changing the preference to Continue,
     # is that Invoke-VstsTaskScript will no longer handle setting the result to failed.
     $global:ErrorActionPreference = 'Continue'
+    
+    $strPurgeContent = $PurgeContent.Replace("`r", "").Replace("`n", "', '")
+    $arrayPurgeContent = $PurgeContent.Replace("`r", "").Split("`n", [System.StringSplitOptions]::RemoveEmptyEntries)
 
-    Write-Host "##[command]Unpublish-AzureRmCdnEndpointContent -ResourceGroupName $ResourceGroupName -ProfileName $ProfileName -EndpointName $EndpointName -PurgeContent $PurgeContent"
-    Unpublish-AzureRmCdnEndpointContent -ResourceGroupName $ResourceGroupName -ProfileName $ProfileName -EndpointName $EndpointName -PurgeContent $PurgeContent
+    Write-Host "##[command] Unpublish-AzureRmCdnEndpointContent -ResourceGroupName $ResourceGroupName -ProfileName $ProfileName -EndpointName $EndpointName -PurgeContent @( '$strPurgeContent' )"
+    Unpublish-AzureRmCdnEndpointContent -ResourceGroupName $ResourceGroupName -ProfileName $ProfileName -EndpointName $EndpointName -PurgeContent $arrayPurgeContent
 
 } finally {
     Trace-VstsLeavingInvocation $MyInvocation
